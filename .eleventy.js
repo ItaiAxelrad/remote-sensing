4af2d258a665b511cdb0ca024031a500a5fb295a
ItaiAxelrad <itaiaxelrad@gmail.com>
2025-01-17 13:27:21 -0700

    chore: bump eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,11 @@
-const pluginNavigation = require('@11ty/eleventy-navigation');
-const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+import pluginNavigation from '@11ty/eleventy-navigation';
+import syntaxHighlight from '@11ty/eleventy-plugin-syntaxhighlight';
 
-module.exports = function (eleventyConfig) {
+/** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
+export default async function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginNavigation);
   eleventyConfig.addPlugin(syntaxHighlight);
-
   eleventyConfig.addPassthroughCopy('data');
   eleventyConfig.addPassthroughCopy('images');
   eleventyConfig.addPassthroughCopy('scripts');
-};
+}
